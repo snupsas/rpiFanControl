10dfca2469b7f5d47340d82edaa9df9b36a8d6e9
root <pontifikas@gmail.com>
2020-01-28 11:20:54 +0200

    Added changes to startup script to be LSB (Linux Standards Base) compliant

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 #
+### BEGIN INIT INFO
+# Provides:          fancontrol
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Example initscript
+# Description:       This file should be used to construct scripts to be
+#                    placed in /etc/init.d.  This example start a
+#                    single forking daemon capable of writing a pid
+#                    file.  To get other behavoirs, implemend
+#                    do_start(), do_stop() or other functions to
+#                    override the defaults in /lib/init/init-d-script.
+### END INIT INFO
 
 # Some things that run always
 set -e
-FANCONTROLDIR=<PATH_GOES_HERE>
+FANCONTROLDIR=/home/benas/gitSources/C/rpiFanControl
 
 # Carry out specific functions when asked to by the system
 case "$1" in
@@ -16,7 +30,7 @@ case "$1" in
     killall fanControl
     ;;
   *)
-    echo "Usage: /etc/init.d/fancontrol {start|stop}"
+    echo "Usage: /etc/init.d/blah {start|stop}"
     exit 1
     ;;
 esac
